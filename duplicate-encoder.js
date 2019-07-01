6b8c1bfc9f587dc0b79450131d2c089621f48a2c
IronHelixx <Davis_Jesse@Yahoo.com>
2019-07-01 13:08:19 -0500

    Create duplicate-encoder.js
    initial commit

---
+++ duplicate-encoder.js
@@ -0,0 +1,25 @@
+function duplicateEncode(word) {
+  const letterArray = word.toLowerCase().split("");
+  let outPutString = [];
+  for (i in letterArray) {
+    outPutString.push(
+      word.toLowerCase().split(letterArray[i]).length - 1 <= 1 ? "(" : ")"
+    );
+  }
+  return outPutString.join("");
+}
+
+/* **
+// Altrenative Solution:
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
+
+** */
